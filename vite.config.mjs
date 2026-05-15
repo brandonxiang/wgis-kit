c6f36fa28477b258d88cde69735e8212e69e172c
brandon.xiang <1542453460@qq.com>
2026-05-15 19:17:11 +0800

    refactor: migrate to vite-plus and update configuration files`

---
+++ vite.config.mjs
@@ -0,0 +1,65 @@
+import { defineConfig } from 'vite-plus';
+
+export default defineConfig({
+  staged: {
+    '*': 'vp check --fix',
+  },
+  fmt: {
+    ignorePatterns: [],
+    indent: {
+      type: 'space',
+      size: 2,
+    },
+    lineWidth: 120,
+    semi: false,
+    quotes: 'single',
+    trailingCommas: 'always',
+  },
+  lint: {
+    plugins: null,
+    categories: {},
+    rules: {
+      'no-unused-vars': 'warn',
+      eqeqeq: 'error',
+      yoda: 'off',
+    },
+    ignorePatterns: ['node_modules/', 'dist/', 'types/', '*.min.js'],
+    settings: {
+      'jsx-a11y': {
+        polymorphicPropName: null,
+        components: {},
+        attributes: {},
+      },
+      next: {
+        rootDir: [],
+      },
+      react: {
+        formComponents: [],
+        linkComponents: [],
+        version: null,
+        componentWrapperFunctions: [],
+      },
+      jsdoc: {
+        ignorePrivate: false,
+        ignoreInternal: false,
+        ignoreReplacesDocs: true,
+        overrideReplacesDocs: true,
+        augmentsExtendsReplacesDocs: false,
+        implementsReplacesDocs: false,
+        exemptDestructuredRootsFromChecks: false,
+        tagNamePreference: {},
+      },
+      vitest: {
+        typecheck: false,
+      },
+    },
+    env: {
+      builtin: true,
+    },
+    globals: {},
+    options: {
+      typeAware: false,
+      typeCheck: false,
+    },
+  },
+});
